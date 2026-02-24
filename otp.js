9e5d57bc1a426918118fb9a574062843b525cc9f
shibindas-2003 <dasshibin09@gmail.com>
2026-02-24 08:37:44 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,8 @@
+//wap to generate 5 digit OTP
+let o1 = Math.floor(Math.random()*10)
+let o2 = Math.floor(Math.random()*10)
+let o3 = Math.floor(Math.random()*10)
+let o4 = Math.floor(Math.random()*10)
+let o5 = Math.floor(Math.random()*10)
+
+console.log("OTP :  ",o1,o2,o3,o4,o5)
